5241ba162bcb6ed37c428e11271bf7fa0c35234c
Florian Schrank <schrank251@web.de>
2024-01-03 01:06:37 +0000

    Enable verbose show background jobs

--- .p10k.zsh
+++ .p10k.zsh
@@ -566,11 +566,12 @@
 
   #######################[ background_jobs: presence of background jobs ]#######################
   # Don't show the number of background jobs.
-  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=false
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=true
   # Background jobs color.
   typeset -g POWERLEVEL9K_BACKGROUND_JOBS_FOREGROUND=1
   # Custom icon.
   typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VISUAL_IDENTIFIER_EXPANSION='%B%%'
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_CONTENT_EXPANSION='%B${P9K_CONTENT}'
 
   #######################[ direnv: direnv status (https://direnv.net/) ]########################
   # Direnv color.
